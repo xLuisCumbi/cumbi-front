1b86cd5ba6f26ef33b8ff0127747e2368c3006dc
Luis Jimenez <LRestrepo@contractor.nexstardigital.com>
2023-09-03 21:10:28 -0500

    Remove clo config

--- src/config/default.jsx
+++ src/config/default.jsx
@@ -4,5 +4,4 @@ const config = {
     businessUrl: import.meta.env.VITE_BUSINESS_API_URL || "http://localhost:5001/api/business",
     settingsUrl: import.meta.env.VITE_SETTINGS_API_URL || "http://localhost:5001/api/settings",
 }
-console.log('config', config);
 export { config };
