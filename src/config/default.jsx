3c4d9df2adfe09da3e47e83116851cc570dcca52
Luis Jimenez <LRestrepo@contractor.nexstardigital.com>
2023-09-28 07:40:16 -0500

    update deposit endpoint

--- src/config/default.jsx
+++ src/config/default.jsx
@@ -3,7 +3,7 @@ const config = {
     apiUrl: import.meta.env.VITE_API_URL || "http://localhost:5001/api/user",
     businessUrl: import.meta.env.VITE_BUSINESS_API_URL || "http://localhost:5001/api/business",
     settingsUrl: import.meta.env.VITE_SETTINGS_API_URL || "http://localhost:5001/api/settings",
-    depositsUrl: import.meta.env.VITE_SETTINGS_API_URL || "http://localhost:5001/api/deposit",
+    depositsUrl: import.meta.env.VITE_DEPOSIT_API_URL || "http://localhost:5001/api/deposit",
     paymentBaseUrl: import.meta.env.APPURL || "http://localhost:5001",
 }
 export { config };
