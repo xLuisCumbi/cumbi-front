0734faac57fb36b772d57467f2709a1557b9cd98
Luis Jimenez <LRestrepo@contractor.nexstardigital.com>
2023-10-11 09:09:07 -0500

    Reorder env vars

--- src/config/default.jsx
+++ src/config/default.jsx
@@ -1,10 +1,9 @@
 const config = {
-
+    paymentBaseUrl: import.meta.env.APPURL || "http://localhost:5001",
     apiUrl: import.meta.env.VITE_API_URL || "http://localhost:5001/api/user",
     businessUrl: import.meta.env.VITE_BUSINESS_API_URL || "http://localhost:5001/api/business",
     settingsUrl: import.meta.env.VITE_SETTINGS_API_URL || "http://localhost:5001/api/settings",
     depositsUrl: import.meta.env.VITE_DEPOSIT_API_URL || "http://localhost:5001/api/deposit",
-    paymentBaseUrl: import.meta.env.APPURL || "http://localhost:5001",
     countryUrl: import.meta.env.VITE_COUNTRY_API_URL || "http://localhost:5001/api/country",
     coinUrl: import.meta.env.VITE_COIN_API_URL || "http://localhost:5001/api/coin",
     bankAccountUrl: import.meta.env.VITE_BANKACCOUNT_API_URL || "http://localhost:5001/api/bankaccount",
